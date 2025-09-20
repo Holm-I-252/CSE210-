6dd658c5733298b79c2139d02f4d16dd5e0a71b6
Ian Holm <ianholm252@gmail.com>
2025-09-19 20:08:39 -0600

    C# Prep assignment 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("James Bond name generator. Enter your first name:");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name:");
+        string lastName = Console.ReadLine();
+
+        string bondName = $"{lastName}, {firstName} {lastName}";
+        Console.WriteLine($"Your name is: {bondName}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Grade Converter. Enter your grade (0-100):");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        string letter = "";
+        string sign = "";
+
+        if (gradeInt >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter != "F")
+        {
+            int lastDigit = gradeInt % 10;
+            if (lastDigit >= 7 && letter != "A")
+            {
+                sign = "+";
+            }
+            else if (lastDigit <= 3)
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
     }
 }
