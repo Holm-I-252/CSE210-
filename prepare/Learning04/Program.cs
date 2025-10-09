3487287f3e32c61ef6c02c83f23dc89a7286fce0
Ian Holm <ianholm252@gmail.com>
2025-10-08 22:13:33 -0600

    Completed Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math_1 = new MathAssignment("Jimmy", "Fractions", "5.1", "1-10");
+        Console.WriteLine(math_1.GetSummary());
+        Console.WriteLine(math_1.GetHomeworkList());
+
+        WritingAssignment writing_1 = new WritingAssignment("Jimmy 2", "European History", "The Causes of World War II");
+        Console.WriteLine(writing_1.GetSummary());
+        Console.WriteLine(writing_1.GetWritingInformation());
     }
 }
