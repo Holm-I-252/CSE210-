3b6d2cf73db5a2f8bd4a61cb014a32e002fe2967
Ian Holm <ianholm252@gmail.com>
2025-10-15 22:23:29 -0600

    Completed Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Square("Green", 3));
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,24 @@
+public class Square : Shape
+{
+    private double _sideLength;
+
+    public Square(string color, double sideLength) : base(color)
+    {
+        _sideLength = sideLength;
+    }
+
+    public double GetSideLength()
+    {
+        return _sideLength;
+    }
+
+    public void SetSideLength(double sideLength)
+    {
+        _sideLength = sideLength;
+    }
+
+    public override double GetArea()
+    {
+        return _sideLength * _sideLength;
+    }
+}
